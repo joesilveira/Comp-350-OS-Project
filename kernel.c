d29921e52eb68c9c86a86af0adb2b321dab6f34d
Alex-Stalter <astalter@student.beridgew.edu>
2019-10-24 09:19:01 -0400

    added print string funtion to kernel.c and compiled it.

--- kernel.c
+++ kernel.c
@@ -1,20 +1,27 @@
+#include <stdio.h>
 
-
+void printString(char*);
 
 void main()
 {
 	int startVidMem = 0xb800;
 	int vidMemOffset = 0x0;
 	int white = 0x7;
-	char* letters = "Hello World\0";
+	char* letters = "Hello World!\0";
 	while(*letters !=0x0){
 		putInMemory(startVidMem,vidMemOffset,*letters);
 		vidMemOffset++;
 		putInMemory(startVidMem,vidMemOffset,white);
 		vidMemOffset++;
 		letters++;
-
 	}
+	interrupt(0x10,0xe*256+'Q',0,0,0);
 	while(1);
+
+}
+
+void printString(char* chars){
+
+
 
 }
