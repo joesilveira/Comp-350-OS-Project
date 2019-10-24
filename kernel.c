f21e1b887546e90cf160b77cd98809f6aef36765
Alex-Stalter <astalter@student.beridgew.edu>
2019-10-24 19:15:42 -0400

    Commented out Hello World from ProjectA

--- kernel.c
+++ kernel.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
 void printString(char*);
+void printChar(char);
+void readString(char*);
+
 
 void main()
 {
-	int startVidMem = 0xb800;
-	int vidMemOffset = 0x0;
-	int white = 0x7;
-	char* letters = "Hello World!\0";
-	while(*letters !=0x0){
-		putInMemory(startVidMem,vidMemOffset,*letters);
-		vidMemOffset++;
-		putInMemory(startVidMem,vidMemOffset,white);
-		vidMemOffset++;
-		letters++;
-	}
-	printString("Hello World!\0");
+//	int startVidMem = 0xb800;
+//	int vidMemOffset = 0x0;
+//	int white = 0x7;
+//	char* letters = "Hello World!\0";
+//	while(*letters !=0x0){
+//		putInMemory(startVidMem,vidMemOffset,*letters);
+//		vidMemOffset++;
+//		putInMemory(startVidMem,vidMemOffset,white);
+//		vidMemOffset++;
+//		letters++;
+//	}
+
+	printString("Hello World! how are you today?\0");
 	while(1);
 
 }
