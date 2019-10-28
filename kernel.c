3f623275459a5bf0c27ed0da6460ffa5d9c7be4f
Alex-Stalter <astalter@student.beridgew.edu>
2019-10-28 14:50:11 -0400

    Finished step2

--- kernel.c
+++ kernel.c
@@ -19,7 +19,11 @@ void main()
 //		letters++;
 //	}
 
-	printString("Hello World! how are you today?\0");
+//	printString("Hello World! how are you today?\0");
+	char* line[80];
+	printString("Enter a line: ");
+	readString(line);
+	printString(line);
 	while(1);
 
 }
@@ -31,5 +35,45 @@ void printString(char* chars){
 	interrupt(0x10,0xe*256+chars[i],0,0,0);
 	i++;
 	}
+
+}
+
+void printChar(char c){
+	interrupt(0x10,0xe*256+c,0,0,0);
+}
+void readString(char* line){
+
+	int i =0;
+	int exit =0;
+	while(exit==0){
+
+	char input = interrupt(0x16,0,0,0,0);
+
+	if(input==0xd){
+	printChar(0xd);
+	line[i]=0xd;
+	i++;
+	line[i]=0xa;
+	i++;
+	printChar(0xa);
+	line[i]=0x0;
+//	printString("Enter has been pressed\0");
+	exit=1;
+
+	}else if(input==0x8){
+	printChar(0x8);
+	printChar(' ');
+	printChar(0x8);
+		if(i>0){
+
+		i--;
+		}
+	}else{
+	line[i]=input;
+	printChar(input);
+	i++;
+	}
+	}
+
 
 }
