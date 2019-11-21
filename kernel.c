b1a4dd907a6f723c37d9774d397b25d184f3c694
Joe Silveira <55754953+joesilveira@users.noreply.github.com>
2019-11-21 11:14:21 -0500

    Update kernel.c
    Re-Write 'readFile' Function
    Added the method 'writeSector' "Working"
    Added the method 'listDir' "Fixing bugs"

--- kernel.c
+++ kernel.c
@@ -1,30 +1,49 @@
 //Alex Stalter
 //also Jack Nichols
+//also joe silveira
 //#include <stdio.h>
 
+
+//String stuff
 void printString(char*);
 void printChar(char);
 void readString(char*);
+
+//File stuff
 void readSector(char*,int);
-void handleInterrupt21(int,int,int,int,int);
 void readFile(char*,char*,int*);
+void writeSector(char*, int);
+void listDir();
+
+//Program stuff
+void handleInterrupt21(int,int,int,int,int);
 void executeProgram(char*);
 void terminate();
+void test();
+
 void main()
 {
-
+	
 	makeInterrupt21();
-	interrupt(0x21,4,"shell",0,0);
+	interrupt(0x21, 4, "shell", 0, 0); 
+
+
 	while(1);
 
 }
 
+
+
+/*
+	************************String Stuff**************************
+
+*/
 void printString(char* chars){
 	int i = 0;
 	while(chars[i]!=0x0){
 
-	interrupt(0x10,0xe*256+chars[i],0,0,0);
-	i++;
+		interrupt(0x10,0xe*256+chars[i],0,0,0);
+		i++;
 	}
 
 }
@@ -32,38 +51,63 @@ void printString(char* chars){
 void printChar(char c){
 	interrupt(0x10,0xe*256+c,0,0,0);
 }
-void readString(char* line){
 
-	int i =0;
-	int exit =0;
-	while(exit==0){
+void readString(char* string){
 
-	char input = interrupt(0x16,0,0,0,0);
-	if(input==0xd){
-	printChar(0xd);
-	printChar(0xa);
-	line[i]=0xd;
-	i++;
-	line[i]=0xa;
-	i++;
-	line[i]=0x0;
+	//local variables
+	int i = 0;
+	int pos = 0;
+	int stringLength = 80;
+	char currentChar = interrupt(0x16, pos, 0, 0, 0);
+	char backPressed = 0x8;
+	char enterPressed = 0xd;
 
-	exit=1;
+	//Start string - 2 and make sure enter isnt pressed
+	while(i < string-2 && currentChar != enterPressed){
 
-	}else if(input==0x8 && i!=0){
-	printChar(0x8);
-	printChar(' ');
-	printChar(0x8);
-	i--;
-	}else if(input!=0x8){
-	line[i]=input;
-	printChar(input);
-	i++;
+		//if backspace isnt pressed
+		if(currentChar != backPressed){
+
+			printChar(currentChar);
+			string[i] = currentChar;
+			i++;
+		}else{
+			//If backspace is pressed
+			i--;
+
+			//if the string length is greater than 0 at interrupt
+			if(i >= 0){
+
+				//Print out the char
+				printChar(currentChar);
+
+				//Change the current position value on interrupt
+				interrupt(0x10, 0xe*256+'\0', 0, 0, 0);
+
+				//Call the bacspace interrupt
+				interrupt(0x10, 0xe*256+backPressed, 0, 0, 0);
+
+			}else{
+				//if the string is not then set iteration back to start
+				i = 0;
+			}
+		}
+		//Now if enter is pressed position to interrupt 
+		//at current and allow no more entry
+		currentChar = interrupt(0x16,0,0, 0, 0);
 	}
-	}
 
-
+	//Add new line feed with carriage return and new line
+	printString("\r\n");
 }
+
+
+
+/*
+	**************************File Stuff*****************
+
+*/
+
 void readSector(char* buffer,int sector){
 
 
@@ -74,112 +118,272 @@ void readSector(char* buffer,int sector){
 
 
 }
-void handleInterrupt21(int ax,int bx,int cx,int dx){
 
-	if(ax==0){
-	printString(bx);
-	}else if(ax==1){
 
-	readString(bx);
+void writeSector(char* buffer, int sector){
+	int ax = 3*256+1;
+	int cx = 0*256+(sector + 1);
+	int dx = 0*256+0x80;
+	interrupt(0x13,ax,buffer,cx,dx);
+}
 
-	}else if(ax==2){
+/*
+*Renewed by Joe Silviera
+*
+*
+//Per Professor Black
+*
+*Parameter 'sectorsRead' may or may not be used
+*
+*Need to add for loop to loop through each sector not just 1 sector
+*
+//Original req's (comments for own use)
+*
+*1. Takes fileName and reads that in to buffer (A.K.A parameters filename and buffer)
+*
+*2. Calls interrupt 21 under the following parameters:
+*
+*	interrupt 21 (ax,bx,cx) formally interrupt 21 (ax,bx,cx,dx)
+*
+*	ax = 3 - interrupt call
+*	bx = file name or address or char array
+*	cx = address of buffer to hold file
+*
+*	//dx = #number of sector to read//
+*
+*	The use of dx is nullified because we now have a for loop that will
+*	loop through each sector until the sector reads '\0' rather than
+*	giving the function and exact number of sectors to read it will determine that on its own
+*
+*
+//Basic operation
+*
+*1. Load file directory in to 512 char array using readSector (Variables: fileDirectory and buffer)
+*
+*2. Loop through the directory 
+*
+*	2.1 Try to match the file name
+*
+*	2.2 If the file name is not found set sectors read to 0 and return
+*
+*		**We will not be using sectors read therefore we will loop**
+*
+*3. Using the sector numbers in the directory:
+*
+*	a: Load the file sector by sector in to the buffer array
+*
+*	b. add 512 to buffer address everytime readSector is called
+*
+*	c. Increment # of sector we read everytime we loop
+*
+*		**Again we will not need to increment as we will loop until '\0'**
+*
+*/
+void readFile(char* fileName , char* fileBuffer) {
 
-	readSector(bx,cx);
+	//Variables
+	char fileDirectory[512];
+	char buffer[512];
 
-	}else if(ax==3){
+		//for loop variables 
 
-	readFile(bx,cx,dx);
-	
-	}else if(ax==4){
+		int i = 0;	//Directory iteration
+		int k = 0;	//File name iteration
+		int j = 0;	//sector iteration
+		int l = 0;	//Inner sector iteration (# sectors)
 
-	executeProgram(bx);
+		int matches = 0;	
 
-	}else if(ax==5){
-	
-	terminate();
+		/*
+			*Function begin
+		*/
 
-	}else{
+		//1. Load file in to 512 char array
+		readSector(buffer,2);
 
-	printString("ERROR no such function exists\n");
+		//2. Loop through the directory 
+		for(i = 0; i < 512; i++){
 
+			//2.1 try to match the file name
+			if(buffer[i] != fileName[matches]){
+
+				//2.2 if it doesnt match go to the next sector
+				i += 31;
+
+			}else if (matches == 5){
+				//printString("Matches \n");
+				
+
+				//Loop through the remaining 26 bytes (sectors)
+				for(j = 0; j < 26; j++){
+
+					//If the next sector is empty (== '\0') set the filebuffer to \0 and break
+					if(buffer[j + i + 1] == '\0'){
+
+						fileBuffer[j * 512] = '\0';
+
+						//printString("setting to 0\n");
+						break;
+
+					//If not read the sector 
+					}else{
+
+					//printString("Reading Sectors\n");
+
+					//Read the sectors and set them equal to the given sector
+						readSector(fileDirectory,buffer[j + i + 1]);
+
+					//3C: This is where we eliminate sectors read and have the for loop perform 
+						for(l = 0; l < 512; l++){
+
+						//Set the passed sector equal to the directory sector
+							fileBuffer[(j * 512) + l] = fileDirectory[l];
+						}
+					}
+				}
+				break;
+
+			//buffer[i] does equal fileBuffer[macthes]	so ++;
+			}else{
+				matches++;
+			}
+
+		}
+		return;
 	}
+
+	
+
+
+
+
+void listDir(){
+
+//local vars
+		// char dirBuffer[512];
+		// char buffer[512];
+		// int i;
+		// int j;
+		// int k = 0;
+
+	//printString("Calling");
+		//readSector(dirBuffer,2);
+	printString("Calling");
+
+
+	// //loop through the sector 
+	// 	for(i = 0; i < 16; i++){
+
+	// 	//if file exists with in the given sector
+	// 		if(dirBuffer[i * 32] != 0x0){
+
+	// 		//Read the file name 
+	// 			for(j = 0; i < 6; j++){
+
+	// 				if(dirBuffer[j] != '\0'){
+
+	// 					buffer[k] = dirBuffer[i * 32 + j];
+	// 					k++;
+
+	// 				}
+
+	// 			}
+	// 		}else{
+	// 			printString("No file");
+	// 		}
+
+	// 	//Print out the directory
+
+	// 		printString("Directory\n");
+	// 		for(i = 0; i < 512; i++){
+	// 			printString(buffer[i]);
+	// 		}
+
+	// 	}
 
 
 }
 
-void readFile(char* address,char* buffer,int* sectorsRead){
-	int count=0;
-	int i=0;
-	int matches = 0;
-	char dir[512];
 
-	readSector(dir,2);
-	for(count=0;count<512;count=count+32)
+
+/*
+	***********************Program Stuff********************
+
+*/
+
+	void handleInterrupt21(int ax,int bx,int cx,int dx){
+
+		if(ax==0){
+			printString(bx);
+		}else if(ax==1){
+
+			readString(bx);
+
+		}else if(ax==2){
+
+			readSector(bx,cx);
+
+		}else if(ax==3){
+
+			readFile(bx,cx);
+
+		}else if(ax==4){
+
+			executeProgram(bx);
+
+		}else if(ax==5){
+
+			terminate();
+
+		}else if(ax==6){
+
+			printString("Calling function");
+
+		}else if (ax == 7){
+			test();
+		}
+
+		else{
+
+			printString("ERROR no such function exists\n");
+
+		}
+
+
+	}
+
+	void executeProgram(char* name)
 	{
+		int i;
+		char buffer[13312];
+		int segment = 0x2000;
 
+		readFile(name, buffer);
 
-	*sectorsRead=*sectorsRead+1;
-
-		for(i=0;i<6;i++)
+		for(i = 0; i < 13312; i++)
 		{
-
-		if(address[i]==dir[i+count]){
-
-		matches++;
-//		printChar(dir[i+count]);
-
-		}else{
-
-		matches=0;
-		break;
+			putInMemory(segment, i, buffer[i]);
 		}
 
-		}
+		launchProgram(segment);
 
-
-	if(matches==6){
-	readSector(buffer,*sectorsRead+4);
-	break;
-
-}
 
 	}
-	if(matches!=6){
-	*sectorsRead=0;
-	printString("/nError file not found.");
-}
 
-}
-void executeProgram(char* name)
-{
-	int i;
-	char buffer[13312];
-	int segment = 0x2000;
-
-	readFile(name, buffer);
-
-	for(i = 0; i < 13312; i++)
+	void terminate()
 	{
-		putInMemory(segment, i, buffer[i]);
+		char shell[6];
+		shell[0] = 's';
+		shell[1] = 'h';
+		shell[2] = 'e';
+		shell[3] = 'l';
+		shell[4] = 'l';
+		shell[5] = '\0';
+		executeProgram(shell);
 	}
 
-	launchProgram(segment);
-
-
-}
-
-void terminate()
-{
-
-	char shell[6];
-	shell[0] = 's';
-	shell[1] = 'h';
-	shell[2] = 'e';
-	shell[3] = 'l';
-	shell[4] = 'l';
-	shell[5] = '\0';
-	executeProgram(shell);
-
-}
+	void test(){
+		printString("This is a test of function 'test()'");
+	}
 
 
