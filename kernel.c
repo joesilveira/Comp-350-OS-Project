63f8bcc417be7aa0edc7d4eca0be8a20e7e8da93
Alex-Stalter <astalter@student.beridgew.edu>
2019-11-26 09:51:26 -0500

    Updated shell.c and kernel.c with listDir and writeSector

--- kernel.c
+++ kernel.c
@@ -8,8 +8,10 @@ void readString(char*);
 void readSector(char*,int);
 void handleInterrupt21(int,int,int,int,int);
 void readFile(char*,char*,int*);
+void writeSector(char*, int);
 void executeProgram(char*);
 void terminate();
+void listDir();
 void main()
 {
 
@@ -74,6 +76,12 @@ void readSector(char* buffer,int sector){
 
 
 }
+void writeSector(char* buffer, int sector){
+	int ax = 3*256+1;
+	int cx = 0*256+(sector + 1);
+	int dx = 0*256+0x80;
+	interrupt(0x13,ax,buffer,cx,dx);
+}
 void handleInterrupt21(int ax,int bx,int cx,int dx){
 
 	if(ax==0){
@@ -98,6 +106,14 @@ void handleInterrupt21(int ax,int bx,int cx,int dx){
 	
 	terminate();
 
+	}else if(ax==6){
+
+	writeSector(bx,cx);
+
+	}else if(ax==7){
+
+	listDir();
+
 	}else{
 
 	printString("ERROR no such function exists\n");
@@ -106,8 +122,8 @@ void handleInterrupt21(int ax,int bx,int cx,int dx){
 
 
 }
-
-void readFile(char* address,char* buffer,int* sectorsRead){
+//old readFile
+/*void readFile(char* address,char* buffer,int* sectorsRead){
 	int count=0;
 	int i=0;
 	int matches = 0;
@@ -148,6 +164,142 @@ void readFile(char* address,char* buffer,int* sectorsRead){
 	*sectorsRead=0;
 	printString("/nError file not found.");
 }
+
+}
+*/
+//New readFile written by Joe Silveira
+void readFile(char* fileName , char* fileBuffer) {
+
+//Variables
+	char fileDirectory[512];
+	char buffer[512];
+
+	//for loop variables 
+
+	int i = 0;	//Directory iteration
+	int k = 0;	//File name iteration
+	int j = 0;	//sector iteration
+	int l = 0;	//Inner sector iteration (# sectors)
+
+	int matches = 0;	
+
+	/*
+		*Function begin
+	*/
+
+	//1. Load file in to 512 char array
+	readSector(buffer,2);
+
+	//2. Loop through the directory 
+	for(i = 0; i < 512; i++){
+
+		//2.1 try to match the file name
+		if(buffer[i] != fileName[matches]){
+
+			//2.2 if it doesnt match go to the next sector
+			i += 31;
+
+		}else if (matches == 5){
+			//printString("Matches \n");
+			
+
+			//Loop through the remaining 26 bytes (sectors)
+			for(j = 0; j < 26; j++){
+
+				//If the next sector is empty (== '\0') set the filebuffer to \0 and break
+				if(buffer[j + i + 1] == '\0'){
+
+					fileBuffer[j * 512] = '\0';
+
+					//printString("setting to 0\n");
+					break;
+
+				//If not read the sector 
+				}else{
+
+				//printString("Reading Sectors\n");
+
+				//Read the sectors and set them equal to the given sector
+					readSector(fileDirectory,buffer[j + i + 1]);
+
+				//3C: This is where we eliminate sectors read and have the for loop perform 
+					for(l = 0; l < 512; l++){
+
+					//Set the passed sector equal to the directory sector
+						fileBuffer[(j * 512) + l] = fileDirectory[l];
+					}
+				}
+			}
+			break;
+
+		//buffer[i] does equal fileBuffer[macthes]	so ++;
+		}else{
+			matches++;
+		}
+
+	}
+	return;
+}
+
+
+
+
+
+
+void listDir(){
+
+
+
+	//local vars
+	char directoryBuffer[512];
+	char buffer[512];
+	int i;
+	int j;
+	int k = 0;
+	int l = 0;
+	int size = 0;
+
+
+	//Set both buffers at i to 0 to start
+	for(i = 0; i < 512; i++){
+		directoryBuffer[i] = 0x0;
+		buffer[i] = 0x0;
+	}
+
+	//Read the given sector of the directory
+	readSector(directoryBuffer,2);
+
+	//loop through the sector 
+	for(i = 0; i < 16; i++){
+
+		//if file exists with in the given sector
+		if(directoryBuffer[32 * i] != '\0'){
+
+			//Read the file name 
+			for(j = 0; j < 6; j++){
+
+				buffer[k] = directoryBuffer[i * 32 + j];
+				k++;		
+				
+
+			}
+
+			//Add a new line and return for every file 
+			//print nicely on its own line
+			buffer[k] = '\r';
+			k++;
+			buffer[k] = '\n';
+			k++;
+			
+		}
+	}
+	printString("\n\r");
+	for(i = 0; i < 512; i++){
+
+		if(buffer[i] != 0x0){
+			printChar(buffer[i]);
+		}	
+	}
 
 }
 void executeProgram(char* name)
